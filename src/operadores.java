2ec6e28ed2e14f6188253a3ddd1d7b358b340a90
rsantosmartins <rsantosmartins@icloud.com>
2023-04-06 17:18:32 -0300

    operadores

---
+++ src/operadores.java
@@ -0,0 +1,12 @@
+package src;
+
+public class operadores {
+  
+  public static void main(String [] args) {
+    String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+    System.out.println(nomeCompleto);
+
+    
+  }
+}
